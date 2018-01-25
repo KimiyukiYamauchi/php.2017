5454a62a19aa4f23881c6e6ccb7f3879f2836d08
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-01-25 21:07:01 +0900

    Calendar完成

--- cal/index.php
+++ cal/index.php
@@ -1,52 +1,16 @@
 <?php
 
-$t = '2018-01';
-$thisMonth = new DateTime($t);  // 2020-08-01
-$yearMonth = $thisMonth->format('F Y');
-
-// var_dump($yearMonth);
+// var_dump($_GET['t']);
 // exit;
 
-$tail = '';
-$lastDayOfPrevMonth = 
-  new DateTime('last day of ' . $yearMonth . '-1 month');
-while($lastDayOfPrevMonth->format('w') < 6) {
-  $tail  = sprintf('<td class="gray">%d</td>',
-            $lastDayOfPrevMonth->format('d')) . $tail;
-  $lastDayOfPrevMonth->sub(new DateInterval('P1D'));
+require 'Calendar.php';
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
 
-$body = '';
+$cal = new \MyApp\Calendar();
 
-$perid = new DatePeriod(
-  new DateTime('first day of ' . $yearMonth),
-  new DateInterval('P1D'),
-  new DateTime('first day of ' . $yearMonth . '+1 month')
-);
-
-$today = new DateTime('today'); // 今日の日付値を取得
-
-foreach ($perid as $day) {
-  if( $day->format('w') === '0') {
-    $body .= '</tr><tr>';
-  }
-  // 追加する日が今日だったら「today」クラスを追加
-  $todayClass = ($day->format('Y-m-d') === $today->format('Y-m-d')) ? 'today' : '';
-  $body .= sprintf('<td class="youbi_%d %s">%d</td>',
-    $day->format('w'), $todayClass, $day->format('d'));
-}
-
-$head = '';
-
-$firstDayOfNextMonth = 
-  new DateTime('first day of ' . $yearMonth . ' +1 month');
-while ($firstDayOfNextMonth->format('w') > 0) {
-  $head .= sprintf('<td class="gray">%d</td>',
-              $firstDayOfNextMonth->format('d'));
-  $firstDayOfNextMonth->add(new DateInterval('P1D'));
-}
-
-$html = '<tr>' . $tail . $body . $head . '</tr>';
 
 ?>
 <!DOCTYPE html>
@@ -60,9 +24,9 @@ $html = '<tr>' . $tail . $body . $head . '</tr>';
   <table>
     <thead>
       <tr>
-        <th><a href="">&laquo;</a></th>
-        <th colspan="5"><?php echo $yearMonth; ?></th>
-        <th><a href="">&raquo;</a></th>
+        <th><a href="/cal/?t=<?php echo h($cal->prev); ?>">&laquo;</a></th>
+        <th colspan="5"><?php echo h($cal->yearMonth); ?></th>
+        <th><a href="/cal/?t=<?php echo h($cal->next); ?>">&raquo;</a></th>
       </tr>
     </thead>
     <tbody>
@@ -75,11 +39,11 @@ $html = '<tr>' . $tail . $body . $head . '</tr>';
         <td>Fri</td>
         <td>Sat</td>
       </tr>
-        <?php echo $html; ?>
+        <?php $cal->show(); ?>
     </tbody>
     <tfoot>
       <tr>
-        <th colspan="7"><a href="">TODAY</a></th>
+        <th colspan="7"><a href="/cal/">TODAY</a></th>
       </tr>
     </tfoot>
 
