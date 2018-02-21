5d2d586bc0979afec7797840fbf498d41ff86c9a
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-02-21 18:03:59 +0900

    twitterボット

---
+++ bot.yo/config.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+define('CONSUMER_KEY', 'IUi5AqAYEzq9cL3mDvgLMAiKo');
+define('CONSUMER_SECRET', 'SOWXhWfwiAGZJZo1Gfof48NXPYDLXR6FJ0RrSd21Xxr2ryoRLn');
+define('ACCESS_TOKEN', '42355689-AGzJFzppg78glaVDuJUPr6wUqaAy9p2IaYXOhb4lO');
+define('ACCESS_TOKEN_SECRET', '0qnfdbsbTCx5ZgEpyjsJ0hsaidKfdbZWnB8c1MmNd2OPD');
